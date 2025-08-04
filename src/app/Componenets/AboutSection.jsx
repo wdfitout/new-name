ce9ebb4b74917badffd96b34181075920efffc12
wdfitout <wdfitout@gmail.com>
2025-08-04 14:36:31 +0400

    asdf

--- src/app/Componenets/AboutSection.jsx
+++ src/app/Componenets/AboutSection.jsx
@@ -85,9 +85,12 @@ export default function AboutSection() {
               <p className="text-sm">
                 Smart Designs, Sharp Results – Choose Buzz Digital Agency for Digital Excellence. At Buzz Digital Agency, we deliver more than just marketing solutions. We create value through innovation, data-driven strategies, and standout content that speaks directly to your target market.
               </p>
+              <p className="text-sm">
+                Smart Designs, Sharp Results – Choose Buzz Digital Agency for Digital Excellence. At Buzz Digital Agency, we deliver more than just marketing solutions. We create value through innovation, data-driven strategies, and standout content that speaks directly to your target market.
+              </p>
               <Link href="/about-us" passHref>
                 <button className="mt-4 bg-[#7D090F] px-4 py-2 rounded-full text-sm font-semibold cursor-pointer">
-                  ABOUT US
+                  ABOUT US - Y Not
                 </button>
               </Link>
             </motion.div>
